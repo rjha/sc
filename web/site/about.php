2768cb70f669f44d79f3b01a2551c2f9af173ba0
Rajeev Jha <jha.rajeev@gmail.com>
2012-03-20 19:30:54 +0530

    put google analytics into site footer, changed footer style, added learn more placeholder link from home tile

---
+++ web/site/about.php
@@ -0,0 +1,75 @@
+<?php
+    include 'sc-app.inc';
+    include($_SERVER['APP_WEB_DIR'] . '/inc/header.inc');
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+       <head>
+        <title> 3mik.com - Learn more  </title>
+        <?php include($_SERVER['APP_WEB_DIR'] . '/inc/meta.inc'); ?>
+         
+        <link rel="stylesheet" type="text/css" href="/3p/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="/css/sc.css">
+		<script type="text/javascript" src="/3p/jquery/jquery-1.7.1.min.js"></script>
+		<script type="text/javascript" src="/3p/bootstrap/js/bootstrap.js"></script>
+		<script type="text/javascript" src="/3p/jquery/masonary/jquery.masonry.min.js"></script>
+
+        <script type="text/javascript" src="/3p/json2.js"></script>
+        <script type="text/javascript" src="/js/sc.js"></script>
+	    
+		
+		<script type="text/javascript">
+            $(document).ready(function(){
+                webgloo.sc.home.addTiles();
+                webgloo.sc.home.addNavGroups();
+           });
+		</script>
+		
+    </head>
+
+     <body class="">
+		<div class="container mh800">
+			<div class="row">
+				<div class="span12">
+					<?php include($_SERVER['APP_WEB_DIR'] . '/inc/toolbar.inc'); ?>
+				</div> 
+				
+			</div>
+			
+			<div class="row">
+				<div class="span12">
+					<?php include($_SERVER['APP_WEB_DIR'] . '/inc/banner.inc'); ?>
+					<?php include($_SERVER['APP_WEB_DIR'] . '/inc/browser.inc'); ?>
+				</div>
+			</div>
+			
+			
+			<div class="row">
+				<div class="span12">
+					<div class="page-header">
+                        <h2> still working on it... </h2>
+					</div>
+
+                    <div id="feedback" class="vertical">
+						<a href="/share/feedback.php">
+							<br>
+                            F e e d b a c k 
+						</a>
+					</div>	<!-- feedback -->
+ 
+				</div> 
+			</div>
+			
+			
+		</div>  <!-- container -->
+              
+       
+        <div id="ft">
+            <?php include($_SERVER['APP_WEB_DIR'] . '/inc/site-footer.inc'); ?>
+        </div>
+
+    </body>
+</html>
