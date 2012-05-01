8ee0c6d52ff050463b6db272a41b6c02aad84266
rjha <jha.rajeev@gmail.com>
2012-05-01 20:14:30 +0530

    Added sendgrid PHP lib + fixed pid file name in site worker

--- db/scripts/site-worker.sh
+++ db/scripts/site-worker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-lf=/var/run/sc.site.worker.pid
+lf=/var/run/sc-site-worker.pid
 touch  $lf
 read lastPID < $lf
 if [ ! -z `echo $lastPID` -a -d /proc/$lastPID ]; then
    echo " site worker already running with PID $lastPID"
    exit
 else
-    echo "started site worker | PID in /var/run/sc.site.worker.pid"
+    echo "started site worker | PID in /var/run/sc-site-worker.pid"
     echo $$ > $lf
     `dirname $0`/site-worker.php
 fi
