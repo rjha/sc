fbb86ad9ce14eb6b89383ea31aba99368237bc84
rjha <jha.rajeev@gmail.com>
2012-08-10 14:20:04 +0530

    item page change: removed fb:like button and zocial button fix

--- web/view/item.php
+++ web/view/item.php
@@ -39,13 +39,7 @@
     </head>
 
     <body class="dark-body">
-        <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-        fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-        </script>
+
 
         <div class="container mh800">
             <div class="row">
@@ -96,23 +90,19 @@
                 </div>
                 <div class="span3 wbg">
 
-                    <div class="section1">
-                        <div class="fb-like p5" data-href="<?php echo $itemObj->netLink;?>" data-send="false" data-layout="button_count" data-width="220" data-show-faces="false"></div>
-
-                        <div class="p5">
-                            <a href="https://twitter.com/share" class="twitter-share-button" data-via="3mikindia" data-count="none">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-                        </div>
-                    </div> <!-- fb:like | tweet -->
-
                     <?php echo \com\indigloo\sc\html\Post::getGroups($postView); ?>
                     <?php echo \com\indigloo\sc\html\Post::getMoreLinks($postView); ?>
 
                     <div class="section social-buttons">
-                        <div class="zocial facebook">
-                            <a href="#" id="share-facebook">share on facebook</a>
+                        <div class="pb10">
+                            <a href="https://twitter.com/share" class="twitter-share-button" data-via="3mikindia" data-count="none">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
                         </div>
-                        <div class="zocial googleplus">
-                               <a href="#" id="share-google">share on google+</a>
+
+                        <div class="pb10">
+                            <a href="#" class="zocial facebook" id="share-facebook">share on facebook</a>
+                        </div>
+                        <div>
+                               <a href="#" class="zocial googleplus" id="share-google">share on google+</a>
                         </div>
 
                     </div>
