f6745ee94553691aeb57ae56b41f504d0720d66c
rjha <jha.rajeev@gmail.com>
2012-08-16 20:50:55 +0530

    added pagination to script

--- db/maintenance/db-to-redis.php
+++ db/maintenance/db-to-redis.php
@@ -1,26 +1,29 @@
 #!/usr/bin/php
 <?php
 
-    /* pre-req: 
-        sc_bookmark, sc_post, sc_comment, sc_follow table should exist
-        turn off site worker cron job (put an exit at top)
-        redis- issue command flushdb - get rid of old keys 
+    /*  
+
+        Before running the script
+        -----------------------------
+        + sc_bookmark, sc_post, sc_comment, sc_follow table should exist
+        + turn off site worker cron job (put an exit at top)
+        + redis-cli : issue command flushdb - get rid of old keys 
         
         After running the script
         -------------------------------
-        remove global job queue and jobs.
+        + remove global job queue and jobs.
         
         del  sc:global:queue:new 
         del  sc:global:jobs
         del sc:global:nextJobId
 
+        + verify using
+        
         exists sc:global:nextJobId
         exists sc:global:jobs
         exists sc:global:queue:new
 
-    
-
-        turn on cron job
+        + turn on cron job
         @todo- do we need to reset sc:global:nextJobId ?
 
     */
@@ -38,130 +41,203 @@
 
     function user_bookmark_to_feed($mysqli) {
         
-        $sql = " select * from sc_bookmark order by id" ;
-        $rows = MySQL\Helper::fetchRows($mysqli,$sql);
+        $sql = "select max(id) as total from sc_bookmark " ;
+        $row = MySQL\Helper::fetchRow($mysqli, $sql);
+        $total = $row["total"] ;
+        $pageSize = 50 ;
+        $pages = ceil($total / $pageSize);
+        $count = 0 ;
 
 
         $userDao = new \com\indigloo\sc\dao\User();
         $postDao = new \com\indigloo\sc\dao\Post();
         $activityDao = new \com\indigloo\sc\dao\ActivityFeed();
 
-        foreach($rows as $row) {
+        while($count  <= $pages ){
+
+            $start =  ($count * $pageSize ) + 1 ;
+            $end = $start + ($pageSize - 1 ) ;
+
+            $sql = " select *  from sc_bookmark where  (id <= {end}) and (id >= {start} ) ";
+            $sql = str_replace(array("{end}", "{start}"),array( 0 => $end, 1=> $start),$sql);
+
+            $rows = MySQL\Helper::fetchRows($mysqli,$sql);
+
+            foreach($rows as $row) {
 
 
-            $loginId = $row['subject_id'];
-            $ownerId = $row['owner_id'];
-            $name = $row['subject'];
-            $itemId = $row['object_id'];
-            $title = $row['object_title'];
-            $title = Util::filterBadUtf8($title);
+                $loginId = $row['subject_id'];
+                $ownerId = $row['owner_id'];
+                $name = $row['subject'];
+                $itemId = $row['object_id'];
+                $title = $row['object_title'];
+                $title = Util::filterBadUtf8($title);
 
-            //number 1|2 stored in table
-            $action = $row['verb'];
+                //number 1|2 stored in table
+                $action = $row['verb'];
 
-            $verb = -1 ;
-            if($action == 1 ) $verb = AppConstants::LIKE_VERB ;
-            if($action == 2 ) $verb = AppConstants::FAVORITE_VERB ;
+                $verb = -1 ;
+                if($action == 1 ) $verb = AppConstants::LIKE_VERB ;
+                if($action == 2 ) $verb = AppConstants::FAVORITE_VERB ;
 
-            $postId = PseudoId::decode($itemId);
-            $image = $postDao->getImageOnId($postId);
+                $postId = PseudoId::decode($itemId);
+                $image = $postDao->getImageOnId($postId);
 
-            //push to redis
-            // get image from item.
-           
-            $postId = PseudoId::decode($itemId);
-            $image = $postDao->getImageOnId($postId);
+                //push to redis
+                // get image from item.
+               
+                $postId = PseudoId::decode($itemId);
+                $image = $postDao->getImageOnId($postId);
 
-            $activityDao->addBookmark($ownerId,$loginId,$name,$itemId,$title,$image,$verb);
+                $activityDao->addBookmark($ownerId,$loginId,$name,$itemId,$title,$image,$verb);
+
+            }
+            
+            sleep(1);
+            $count++ ;
 
         }
-
     }
 
     function follower_to_feed($mysqli) {
-        $sql = " select * from sc_follow order by id" ;
-        $rows = MySQL\Helper::fetchRows($mysqli,$sql);
+
+        $sql = "select max(id) as total from sc_follow" ;
+        $row = MySQL\Helper::fetchRow($mysqli, $sql);
+        $total = $row["total"] ;
+        $pageSize = 50 ;
+        $pages = ceil($total / $pageSize);
+        $count = 0 ;
 
         $userDao = new \com\indigloo\sc\dao\User();
         $activityDao = new \com\indigloo\sc\dao\ActivityFeed();
 
-        foreach($rows as $row) {
-            $followerId = $row['follower_id'] ;
-            $followingId = $row['following_id'] ;
 
-            $row1 = $userDao->getOnLoginId($followerId);
-            $row2 = $userDao->getOnLoginId($followingId);
-            $followerName = $row1['name'];
-            $followingName = $row2['name'];
-            $verb = AppConstants::FOLLOWING_VERB ;
+        while($count  <= $pages ){
 
-            $activityDao->addFollower($followerId, $followerName, $followingId, $followingName, $verb);
+            $start =  ($count * $pageSize ) + 1 ;
+            $end = $start + ($pageSize - 1 ) ;
 
+            $sql = " select *  from sc_follow where  (id <= {end}) and (id >= {start} ) ";
+            $sql = str_replace(array("{end}", "{start}"),array( 0 => $end, 1=> $start),$sql);
+            $rows = MySQL\Helper::fetchRows($mysqli,$sql);
+  
+            foreach($rows as $row) {
+                $followerId = $row['follower_id'] ;
+                $followingId = $row['following_id'] ;
+
+                $row1 = $userDao->getOnLoginId($followerId);
+                $row2 = $userDao->getOnLoginId($followingId);
+                $followerName = $row1['name'];
+                $followingName = $row2['name'];
+                $verb = AppConstants::FOLLOWING_VERB ;
+
+                $activityDao->addFollower($followerId, $followerName, $followingId, $followingName, $verb);
+
+            }
+
+            sleep(1);
+            $count++ ;
         }
     }
 
+
     function comment_to_feed($mysqli) {
-        $sql = " select * from sc_comment order by id " ;
-        $rows = MySQL\Helper::fetchRows($mysqli,$sql);
+
+        $sql = "select max(id) as total from sc_comment" ;
+        $row = MySQL\Helper::fetchRow($mysqli, $sql);
+        $total = $row["total"] ;
+        $pageSize = 50 ;
+        $pages = ceil($total / $pageSize);
+        $count = 0 ;
 
         $userDao = new \com\indigloo\sc\dao\User();
         $activityDao = new \com\indigloo\sc\dao\ActivityFeed();
         $postDao = new \com\indigloo\sc\dao\Post();
 
-        foreach($rows as $row) {
-            $postId = $row['post_id'];
-            $postDBRow = $postDao->getOnId($postId);
-            $image = $postDao->getImageOnId($postId);
+        while($count  <= $pages ){
+
+            $start =  ($count * $pageSize ) + 1 ;
+            $end = $start + ($pageSize - 1 ) ;
+
+            $sql = " select *  from sc_comment where  (id <= {end}) and (id >= {start} ) ";
+            $sql = str_replace(array("{end}", "{start}"),array( 0 => $end, 1=> $start),$sql);
+            $rows = MySQL\Helper::fetchRows($mysqli,$sql);
+          
+            foreach($rows as $row) {
+                $postId = $row['post_id'];
+                $postDBRow = $postDao->getOnId($postId);
+                $image = $postDao->getImageOnId($postId);
 
 
-            $loginId = $row['login_id'];
-            $userDBRow = $userDao->getOnLoginId($loginId);
-            $name = $userDBRow['name'];
-            $ownerId = $postDBRow['login_id'];
-            $itemId = PseudoId::encode($postId);
+                $loginId = $row['login_id'];
+                $userDBRow = $userDao->getOnLoginId($loginId);
+                $name = $userDBRow['name'];
+                $ownerId = $postDBRow['login_id'];
+                $itemId = PseudoId::encode($postId);
 
-            $title = $row['title'] ;
-            $title = Util::filterBadUtf8($title);
+                $title = $row['title'] ;
+                $title = Util::filterBadUtf8($title);
 
-            $content = $row['description'];
-            $content = Util::filterBadUtf8($content);
+                $content = $row['description'];
+                $content = Util::filterBadUtf8($content);
 
-            $verb = AppConstants::COMMENT_VERB ;
-            $activityDao->addComment($ownerId, $loginId, $name, $itemId, $title, $content, $image, $verb);
+                $verb = AppConstants::COMMENT_VERB ;
+                $activityDao->addComment($ownerId, $loginId, $name, $itemId, $title, $content, $image, $verb);
 
+            }
+
+            sleep(1);
+            $count++ ;
         }
 
     }
 
     function post_to_feed($mysqli) {
         
-        $sql = " select * from sc_post order by id " ;
-        $rows = MySQL\Helper::fetchRows($mysqli,$sql);
+        $sql = "select max(id) as total from sc_post" ;
+        $row = MySQL\Helper::fetchRow($mysqli, $sql);
+        $total = $row["total"] ;
+        $pageSize = 50 ;
+        $pages = ceil($total / $pageSize);
+        $count = 0 ;
 
         $userDao = new \com\indigloo\sc\dao\User();
         $activityDao = new \com\indigloo\sc\dao\ActivityFeed();
         $postDao = new \com\indigloo\sc\dao\Post();
 
-        foreach($rows as $row) {
+        while($count  <= $pages ){
 
-            $loginId = $row['login_id'];
-            $postId = $row['id'];
-            $itemId = PseudoId::encode($postId);
-            $image = $postDao->getImageOnId($postId);
+            $start =  ($count * $pageSize ) + 1 ;
+            $end = $start + ($pageSize - 1 ) ;
 
-           
-            $userDBRow = $userDao->getOnLoginId($loginId);
-            $name = $userDBRow['name'];
+            $sql = " select *  from sc_post where  (id <= {end}) and (id >= {start} ) ";
+            $sql = str_replace(array("{end}", "{start}"),array( 0 => $end, 1=> $start),$sql);
+            $rows = MySQL\Helper::fetchRows($mysqli,$sql);
 
-            $title = $row['title'] ;
-            $title = Util::filterBadUtf8($title);
+            foreach($rows as $row) {
 
-            $verb = \com\indigloo\sc\Constants::POST_VERB ;
-            $activityDao->addPost($loginId, $name, $itemId, $title,$image,$verb);
+                $loginId = $row['login_id'];
+                $postId = $row['id'];
+                $itemId = PseudoId::encode($postId);
+                $image = $postDao->getImageOnId($postId);
+    
+                $userDBRow = $userDao->getOnLoginId($loginId);
+                $name = $userDBRow['name'];
 
+                $title = $row['title'] ;
+                $title = Util::filterBadUtf8($title);
+
+                $verb = \com\indigloo\sc\Constants::POST_VERB ;
+                $activityDao->addPost($loginId, $name, $itemId, $title,$image,$verb);
+
+            }
+
+            sleep(1);
+            $count++ ;
         }
 
     }
+
 
     $mysqli = MySQL\Connection::getInstance()->getHandle();
     user_bookmark_to_feed($mysqli);
