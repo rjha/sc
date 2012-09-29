b45e90578f4d510f11d7f4f166eac0595fc727a3
rjha <jha.rajeev@gmail.com>
2012-09-29 20:43:58 +0530

    merged mysql folder from main branch

--- deploy/mysql/mysql.newdb.sql
+++ deploy/mysql/mysql.newdb.sql
@@ -1,6 +1,2 @@
-create database yourdb  character set utf8 collate utf8_general_ci ;
-
-
-grant all privileges on yourdb.* to 'gloo'@'localhost' identified by 'password' with grant option;
-grant all privileges on yourdb.* to 'gloo'@'10.178.225.240' identified by 'password' with grant option;
-grant all privileges on yourdb.* to 'gloo'@'10.183.10.32' identified by 'password' with grant option;
+create database scdb1  character set utf8 collate utf8_general_ci ;
+grant all privileges on scdb1.* to 'user'@'localhost' identified by 'password' with grant option;
