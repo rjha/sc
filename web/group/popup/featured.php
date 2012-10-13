acbaa852f5ed0076cee90710c56d904a8f449438
rjha <jha.rajeev@gmail.com>
2012-10-13 14:06:19 +0530

    bug fix: get featured groups from sc_glob_table

--- web/group/popup/featured.php
+++ web/group/popup/featured.php
@@ -2,13 +2,18 @@
     include ('sc-app.inc');
     include(APP_WEB_DIR . '/inc/header.inc');
 
+    use \com\indigloo\sc\util\Nest as Nest ;
     //public data
     //do not put user role check here
 
     set_exception_handler('webgloo_ajax_exception_handler');
 
     $groupDao = new \com\indigloo\sc\dao\Group();
-    $feature_slug = $groupDao->getFeatureSlug();
+    $collectionDao = new \com\indigloo\sc\dao\Collection();
+    
+    $row = $collectionDao->glget(Nest::fgroups());
+    $feature_slug = empty($row) ? "" : $row["t_value"] ;
+
     $fgroups = $groupDao->slugToGroupsMap($feature_slug);
     $html = \com\indigloo\sc\html\Group::getCloud($fgroups); 
     echo $html;
