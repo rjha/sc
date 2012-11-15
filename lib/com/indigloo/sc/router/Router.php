b718d7b7b7b7b4d4d67d27241c2e5a4835c191cb
rjha <jha.rajeev@gmail.com>
2012-11-15 16:16:03 +0530

    Add routing rule for public lists

--- lib/com/indigloo/sc/router/Router.php
+++ lib/com/indigloo/sc/router/Router.php
@@ -15,19 +15,22 @@ namespace com\indigloo\sc\router{
         function initTable() {
             $this->createRule('^/$', 'com\indigloo\sc\controller\Home');
             $this->createRule('^home/page/(?P<gpage>\d+)$', 'com\indigloo\sc\controller\Home');
-            $this->createRule('^item/(?P<item_id>\d+)$','com\indigloo\sc\controller\Post');
             
-            //rule for item folder. @imp do not use /item/ on our site
+            $this->createRule('^item/(?P<item_id>\d+)$','com\indigloo\sc\controller\Post');
+            //rule for item folder. @imp do not use /item/ inside our htdocs
             $this->createRule('^recent$','com\indigloo\sc\controller\ItemsMap');
+            
             $this->createRule('^category/(?P<category_id>\d+)$','com\indigloo\sc\controller\Category');
             $this->createRule('^search/site$','com\indigloo\sc\controller\Search');
             
-            //rule for group folder. @imp do not use /group/ on our site
+            //rule for group folder. @imp do not use /group/ inside our htdocs
             $this->createRule('^groups/recent$','com\indigloo\sc\controller\GroupsMap');
             $this->createRule('^groups/featured$','com\indigloo\sc\controller\GroupsMap');
             $this->createRule('^group/(?P<name>[-\w]+)$','com\indigloo\sc\controller\Group');
             
             $this->createRule('^pub/user/(?P<login_id>\d+)$','com\indigloo\sc\controller\User');
+            $this->createRule('^pub/list/(?P<list_id>\d+)/(?P<name>[-\w]+)$','com\indigloo\sc\controller\Lists');
+            
             $this->createRule('^search/location/(?P<location>\w+)$','com\indigloo\sc\controller\Location');
             $this->createRule('^surprise/me$','com\indigloo\sc\controller\Random');
             
